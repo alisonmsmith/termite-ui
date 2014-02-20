385103d1baca1e5fc0aa2e3215c5ca0fc5227b8b
Jason Chuang <jcchuang@cs.stanford.edu>
2014-02-19 22:20:07 -0800

    Added a bash script to launch a python web server on 127.0.0.1:8000.

---
+++ start_localhost.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo
+echo "The group-in-a-box visualization is now at http://127.0.0.1:8000/"
+echo "Press Ctrl+C to close this python web server."
+echo
+python -m SimpleHTTPServer 8000
