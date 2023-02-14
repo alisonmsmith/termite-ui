5c269f4366323fa7375cff0edf84527cad0d5a59
Yifei Zhang <yifei7.zhang@gmail.com>
2023-02-13 22:05:28 -0500

    swap to py3 and add explicit package versions

--- start_localhost.sh
+++ start_localhost.sh
@@ -23,7 +23,7 @@ launch_localhost() {
 	echo "Starting a python SimpleHTTPServer at port 8000..."
 	echo "Press Ctrl+C to close this python web server."
 	echo
-	cd public_html && python -m SimpleHTTPServer 8000
+	cd public_html && python -m http.server 8000
 }
 
 update_dependencies
